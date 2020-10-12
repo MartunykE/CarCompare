d8451ee2167ecfcade87077ca67a66aa0f45c45f
Evgen <martunyke@gmail.com>
2020-10-12 20:28:10 +0300

    Setup EF, initial migration to db

---
+++ src/Services/Cars/Cars.API/Models/Manufacturer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.API.Models
+{
+    public class Manufacturer
+    {
+        [Required]
+        public int ManufacturerId { get; set; }
+        [Required]
+        public string MaufacturerName { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
